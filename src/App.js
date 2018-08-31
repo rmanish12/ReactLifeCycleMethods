167c68714a507670a318dd1dd54c09ddeec666e3
Ranjan <manish.ranjan@metronom-external.com>
2018-08-31 12:43:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import Main from './Components/Main'
-import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
     return(
-        <BrowserRouter>
             <Main/>
-        </BrowserRouter>
     )
 }
 
